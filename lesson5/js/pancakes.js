e5768ad45383f5aaaad39084a0b4cb0b6ccd711f
VasilyBelan <bel18004@byui.edu>
2020-05-26 00:00:48 +0500

    1

--- lesson5/js/pancakes.js
+++ lesson5/js/pancakes.js
@@ -1,4 +1,4 @@
-/*if(dayName == 'Friday') {
+if(dayName == 'Friday') {
     document.getElementById("pancakes").innerHTML = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the City Park Pavilion.";
     document.getElementById("pancakes").classList.add("show");
-*/
+}
