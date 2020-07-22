b3e0a46753beb67a2a4a6561b80f2f947397885a
VasilyBelan <bel18004@byui.edu>
2020-07-22 16:08:52 +0500

    1

--- scoots/js/lastUpdateAndToggleMenu.js
+++ scoots/js/lastUpdateAndToggleMenu.js
@@ -2,4 +2,7 @@ var lastUpdate = document.lastModified
 document.getElementById('lastUpdate').innerHTML = 'Last Update '+ lastUpdate
 var year = new Date();
 document.getElementById("year").innerHTML = year.getFullYear();
-
+function toggleMenu() {
+    document.getElementsByClassName('navigation')[0].classList.toggle('responsive');
+}
+var lastUpdate = document.lastModified
--- scoots/js/toggleMenu.js
+++
@@ -1,4 +0,0 @@
-function toggleMenu() {
-    document.getElementsByClassName('navigation')[0].classList.toggle('responsive');
-}
-var lastUpdate = document.lastModified
